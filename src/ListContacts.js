6f121b3c36ddcf3dc64c48a0947c2b26edea4024
Kelly Ng <kellyng87@gmail.com>
2017-07-06 12:46:00 -0700

    Add a controlled component that is ListContacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -38,27 +38,44 @@ class ListContacts extends Component {
 
   }
 
+  updateQuery = (query) => {
+
+    this.setState({ query: query.trim() })
+  }
+
   render() {
     return(
       // return an ordered list
       // maps over each contact and display a contact to the view
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
-          // why do we need a key here?  Because when we do a map, if we don't have a unique identifier for every key, React runs the risk of updating the entire list upon a state change
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}/>
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
+      <div className='list-contacts'>
+      {JSON.stringify(this.state)}
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className='contact-list'>
+          {this.props.contacts.map((contact) => (
+            // why do we need a key here?  Because when we do a map, if we don't have a unique identifier for every key, React runs the risk of updating the entire list upon a state change
+            <li key={contact.id} className='contact-list-item'>
+              <div className='contact-avatar' style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}/>
+              <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     )
   }
 }
